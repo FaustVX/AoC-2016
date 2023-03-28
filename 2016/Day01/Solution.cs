f8a093a7fbbc63ad8ddaef0b8045dbf4548f17db
FaustVX <jodu035@gmail.com>
2023-03-28 17:57:21 +0200

    Initial commit for Y2016D1

---
+++ 2016/Day01/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2016.Day01;
+
+[ProblemName("No Time for a Taxicab")]
+public class Solution : ISolver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
