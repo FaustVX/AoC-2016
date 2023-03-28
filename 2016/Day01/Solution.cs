551b96fbedd3238f11b327f81bd35d3b3a3b38d9
FaustVX <jodu035@gmail.com>
2023-03-28 18:56:34 +0200

    Simplyfied P1

--- 2016/Day01/Solution.cs
+++ 2016/Day01/Solution.cs
@@ -12,27 +12,16 @@ public class Solution : ISolver //, IDisplay
         while (true)
         {
             var comma = span.IndexOf(',');
-            switch ((span[0], orientation), int.Parse(comma is -1 ? span[1..] : span[1..comma]))
+            orientation = (span[0], orientation) switch
             {
-                case (('L', Orientation.North) or ('R', Orientation.South), var dist):
-                    orientation = Orientation.West;
+                ('L', Orientation.North) or ('R', Orientation.South) => Orientation.West,
+                ('L', Orientation.East) or ('R', Orientation.West) => Orientation.North,
+                ('L', Orientation.South) or ('R', Orientation.North) => Orientation.East,
+                ('L', Orientation.West) or ('R', Orientation.East) => Orientation.South,
+                _ => throw new UnreachableException(),
+            };
+            var dist = int.Parse(comma is -1 ? span[1..] : span[1..comma]);
                     pos = Move(orientation, dist, pos);
-                    break;
-                case (('L', Orientation.East) or ('R', Orientation.West), var dist):
-                    orientation = Orientation.North;
-                    pos = Move(orientation, dist, pos);
-                    break;
-                case (('L', Orientation.South) or ('R', Orientation.North), var dist):
-                    orientation = Orientation.East;
-                    pos = Move(orientation, dist, pos);
-                    break;
-                case (('L', Orientation.West) or ('R', Orientation.East), var dist):
-                    orientation = Orientation.South;
-                    pos = Move(orientation, dist, pos);
-                    break;
-                default:
-                    throw new UnreachableException();
-            }
             if (comma is -1)
                 return Math.Abs(pos.x) + Math.Abs(pos.y);
             span = span[(comma + 2)..];
