2ce6c21e0b30161f7b377cd0d2a3cc710ab713f8
FaustVX <jodu035@gmail.com>
2023-03-29 12:06:53 +0200

    Solved P1 in 0:08:34

--- 2016/Day03/Solution.cs
+++ 2016/Day03/Solution.cs
@@ -6,8 +6,18 @@ public class Solution : ISolver //, IDisplay
 {
     public object PartOne(string input)
     {
-        return 0;
+        var count = 0;
+        foreach (var line in input.AsSpan().EnumerateLines())
+        {
+            var (a, b, c) = ParseTriangle(line);
+            if (a + b > c && a + c >= b && b + c > a)
+                count++;
+        }
+        return count;
     }
+
+    static (int, int, int) ParseTriangle(ReadOnlySpan<char> span)
+    => (int.Parse(span.Slice(0, 5)), int.Parse(span.Slice(5, 5)), int.Parse(span.Slice(10, 5)));
 
     public object PartTwo(string input)
     {
