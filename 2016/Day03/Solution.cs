1d189e39e43fe8df8509a8321db27a27b6b2f2bc
FaustVX <jodu035@gmail.com>
2023-03-29 16:57:40 +0200

    Solved P2 in 4:50:42

--- 2016/Day03/Solution.cs
+++ 2016/Day03/Solution.cs
@@ -10,7 +10,7 @@ public class Solution : ISolver //, IDisplay
         foreach (var line in input.AsSpan().EnumerateLines())
         {
             var (a, b, c) = ParseTriangle(line);
-            if (a + b > c && a + c >= b && b + c > a)
+            if (IsTriangle(a, b, c))
                 count++;
         }
         return count;
@@ -19,8 +19,39 @@ public class Solution : ISolver //, IDisplay
     static (int, int, int) ParseTriangle(ReadOnlySpan<char> span)
     => (int.Parse(span.Slice(0, 5)), int.Parse(span.Slice(5, 5)), int.Parse(span.Slice(10, 5)));
 
+    static bool IsTriangle(int a, int b, int c)
+    => a + b > c && a + c > b && b + c > a;
+
+    static bool IsTriangle<T>(Span<T> span, Func<T, int> func)
+    => IsTriangle(func(span[0]), func(span[1]), func(span[2]));
+
+    static bool ParseTriangles(scoped Span<(int a, int b, int c)> triangles, ref SpanLineEnumerator enumerator)
+    {
+        if (!enumerator.MoveNext())
+            return false;
+        triangles[0] = ParseTriangle(enumerator.Current);
+        enumerator.MoveNext();
+        triangles[1] = ParseTriangle(enumerator.Current);
+        enumerator.MoveNext();
+        triangles[2] = ParseTriangle(enumerator.Current);
+        return true;
+    }
+
     public object PartTwo(string input)
     {
-        return 0;
+        var count = 0;
+        Span<(int a, int b, int c)> triangles = stackalloc (int, int, int)[3];
+        var enumerator = input.AsSpan().EnumerateLines();
+
+        while (ParseTriangles(triangles, ref enumerator))
+        {
+            if (IsTriangle(triangles, static tri => tri.a))
+                count++;
+            if (IsTriangle(triangles, static tri => tri.b))
+                count++;
+            if (IsTriangle(triangles, static tri => tri.c))
+                count++;
+        }
+        return count;
     }
 }
