6b73fcdb79d6b08f64e8c920ca7a2b0610f506d9
FaustVX <jodu035@gmail.com>
2023-03-30 19:24:47 +0200

    Initial commit for Y2016D6

---
+++ 2016/Day06/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2016.Day06;
+
+[ProblemName("Signals and Noise")]
+public class Solution : ISolver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
