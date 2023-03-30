ba19b665c0c799bd5569c42801b09de55dae6723
FaustVX <jodu035@gmail.com>
2023-03-30 21:23:37 +0200

    Initial commit for Y2016D7

---
+++ 2016/Day07/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2016.Day07;
+
+[ProblemName("Internet Protocol Version 7")]
+public class Solution : ISolver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
