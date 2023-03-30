4e3d5f30300a914faffe5189d65503315fdbf711
FaustVX <jodu035@gmail.com>
2023-03-30 19:47:15 +0200

    Solved P1 in 0:22:23

--- 2016/Day06/Solution.cs
+++ 2016/Day06/Solution.cs
@@ -6,7 +6,29 @@ public class Solution : ISolver //, IDisplay
 {
     public object PartOne(string input)
     {
-        return 0;
+        var length = input.IndexOf('\n');
+        Span<char> word = stackalloc char[length];
+        for (int i = 0; i < length; i++)
+            word[i] = FindMostCommon(input, i, length + 1);
+        return new string(word);
+    }
+
+    static char FindMostCommon(ReadOnlySpan<char> span, int column, int length)
+    {
+        var most = (c: '\0', count: 0);
+        for (var i = column; i < span.Length; i += length)
+            if (Count(span, column, length, span[i]) is var c && c > most.count)
+                most = (span[i], c);
+        return most.c;
+    }
+
+    static int Count(ReadOnlySpan<char> span, int column, int length, char c)
+    {
+        var count = 0;
+        for (var i = column; i < span.Length; i += length)
+            if (span[i] == c)
+                count++;
+        return count;
     }
 
     public object PartTwo(string input)
