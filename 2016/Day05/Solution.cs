16f6828e7e06dec417e550652c6a3b232e295b0c
FaustVX <jodu035@gmail.com>
2023-03-30 13:08:35 +0200

    Initial commit for Y2016D5

---
+++ 2016/Day05/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2016.Day05;
+
+[ProblemName("How About a Nice Game of Chess?")]
+public class Solution : ISolver //, IDisplay
+{
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
